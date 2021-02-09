d7c855262c24c0381afd89a689410d33b3bffdc6
XufengRen <tuk39898@temple.edu>
2021-02-09 17:14:51 -0500

    button added

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,6 +3,9 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //onclicklistener for button
+        Button button = findViewById(R.id.my_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                TextView t = (TextView)findViewById(R.id.textView);
+                t.setText("You have clicked the button");
+
+            }
+        });
     }
 }
